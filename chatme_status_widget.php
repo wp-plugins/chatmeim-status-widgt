a21d1f3eee08759c24fa35327854dd3a77ae99ab
camaran <camaran@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-08-13 18:18:56 +0000

    0.1 beta 1 first public edition
    git-svn-id: https://plugins.svn.wordpress.org/chatmeim-status-widgt/trunk@584957 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ chatme_status_widget.php
@@ -0,0 +1,91 @@
+<?php
+/*
+Plugin Name: ChatMe Status Widget
+Plugin URI: http://chatme.im
+Description: Displays the ChatMe User Status.
+Author: camaran 
+Version: 0.1 beta 1
+Author URI: http://chatme.im
+*/
+class chatme_status_Widget extends WP_Widget {
+
+	//
+	//	Constructor
+	//
+	function chatme_status_Widget() {
+
+		//	'widget_chatme_status' is the CSS class name assigned to the widget
+		//	'description' is the widget description that appears in the 'Available Widgets' list in the backend
+		$widget_ops = array('classname' => 'widget_chatme_status', 'description' => __('Display the ChatMe User Status') );
+		
+		//	'status-picture-widget', this will be the ID (random-picture-widget-1, random-picture-widget-2, etc)
+		//	__('ChatMe Status Picture') is the title of the widget in the backend
+		$this->WP_Widget('status-picture-widget', __('ChatMe Status Picture'), $widget_ops);
+		
+	}
+	
+	//
+	//	widget() - outputs the content of the widget, in our case: a random picture. 
+	//
+	function widget($args,$instance) {
+	
+		extract($args);
+
+		//	Get the title of the widget and the specified width of the image
+		$title = empty($instance['title']) ? ' ' : apply_filters('widget_title', $instance['title']);
+			
+		//	Outputs the widget in its standard ul li format.
+		echo $before_widget;
+		if (!empty( $title )) { 
+			echo $before_title . 'ChatMe.im Status' . $after_title; 
+		};
+		echo '<ul style="list-style:none;margin-left:0px;">';
+		
+		//	Let's display the image(s)
+
+			//	Outputs the image
+			echo '  <li>'. $title .'@chatme.im <img src="http://chatme.im/plugins/presence/status?jid='.$title.'@chatme.im" alt="ChatMe Status" /></li>';
+		
+		echo '</ul>';
+		echo $after_widget;
+		//	Done
+	}
+	
+	//
+	//	update() - processes widget options to be saved.
+	//
+	function update($new_instance, $old_instance) {
+		
+		$instance = $old_instance;
+		$instance['title'] = strip_tags($new_instance['title']);
+		//$instance['width'] = strip_tags($new_instance['width']);
+
+		return $instance;
+		
+	}
+	
+	//
+	//	form() - outputs the options form on admin in Appearance => Widgets (backend). 
+	//
+	function form($instance) {
+
+		//	Assigns values
+		//$instance = wp_parse_args( (array) $instance, array( 'title' => '', 'width' => '' ) );
+		$instance = wp_parse_args( (array) $instance, array( 'title' => '' ) );
+		$title = strip_tags($instance['title']);
+		//$width = strip_tags($instance['width']);
+		?>
+			<p><label for="<?php echo $this->get_field_id('title'); ?>"><?php echo __('ChatMe Username'); ?>: <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo attribute_escape($title); ?>" />@chatme.im</label></p>
+			<!-- <p><label for="<?php echo $this->get_field_id('width'); ?>"><?php echo __('Width'); ?>: <input class="widefat" id="<?php echo $this->get_field_id('width'); ?>" name="<?php echo $this->get_field_name('width'); ?>" type="text" value="<?php echo attribute_escape($width); ?>" /></label></p> -->
+		<?php
+		
+	}
+
+}
+
+//
+//	Register the chatme_status_Widget widget class
+//
+add_action('widgets_init', create_function('', 'return register_widget("chatme_status_Widget");'));
+
+?>
---
+++ readme.txt
@@ -0,0 +1,58 @@
+=== Chatme.im Status Widget ===
+Contributors: camaran
+Donate link: http://www.chatme.im
+Tags: chat, jabber, jappix, mini, xmpp, status, widget
+Requires at least: 3.3
+Tested up to: 3.4
+Stable tag: 0.1 beta 1
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+This plugin add a widget to show ChatMe.im user status 
+
+== Description ==
+
+This plugin add a widget to show ChatMe.im user status
+
+For more information, check out [chatme.im](http://chatme.im/) and [webchat](https://webchat.chatme.im/).
+
+Special Thanks:
+
+* Neustradamus for french translation
+* To my professor for moral support
+* To my sister for having tolerated
+* My doctor for support
+* My work for the economic support
+
+Hand-crafted, and made with love, in [Rosolina](http://rosolina.me), Italy.
+
+Based on [Jappix](https://project.jappix.com/) and [Jappix Mini](https://mini.jappix.com/).
+
+You can contrib in [GitHub](https://github.com/camaran/chatme.im-mini)
+
+== Installation ==
+
+This section describes how to install the plugin and get it working.
+
+1. Upload this directory to your plugins directory. It will create a 'wp-content/plugins/chatmeim-status-widget/' directory or install it via the WordPress.org plugin directory.
+2. Activate the plugin through the 'Plugins' menu in WordPress
+
+== Frequently Asked Questions ==
+
+= A question that someone might have =
+
+An answer to that question.
+
+== Screenshots ==
+
+1. Status Widget
+
+== Changelog ==
+
+= 1.0 beta 1 =
+* First public version
+
+== Upgrade Notice ==
+
+= 1.0 beta 1 =
+* First public version
